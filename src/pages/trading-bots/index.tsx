56a070a189da20b306df316d70b4a5cda1c8ccc1
khalid20192019 <khalidjosh90@gmail.com>
2026-08-08 11:06:22 +0300

    Update index.tsx

--- src/pages/trading-bots/index.tsx
+++ src/pages/trading-bots/index.tsx
@@ -1,1 +1,19 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import { Localize } from '@deriv-com/translations';
+import './trading-bots.scss';
 
+const TradingBots = observer(() => {
+    return (
+        <div className='trading-bots'>
+            <h2>
+                <Localize i18n_default_text='Trading Bots' />
+            </h2>
+            <p>
+                <Localize i18n_default_text='Browse and manage your trading bots here.' />
+            </p>
+        </div>
+    );
+});
+
+export default TradingBots;
