0dc9758cf63297327543d91f6f3ec08698edfd21
khalid20192019 <khalidjosh90@gmail.com>
2026-08-08 12:37:27 +0300

    Update bot-contents.ts

--- src/constants/bot-contents.ts
+++ src/constants/bot-contents.ts
@@ -17,10 +17,18 @@ export const DBOT_TABS: TDashboardTabIndex = Object.freeze({
     CHART: 2,
     TUTORIAL: 3,
     TRADING_BOTS: 4,
+    ANALYSIS_TOOL: 5,
 });
 
 export const MAX_STRATEGIES = 10;
 
-export const TAB_IDS = ['id-dbot-dashboard', 'id-bot-builder', 'id-charts', 'id-tutorials', 'id-trading-bots'];
+export const TAB_IDS = [
+    'id-dbot-dashboard',
+    'id-bot-builder',
+    'id-charts',
+    'id-tutorials',
+    'id-trading-bots',
+    'id-analysis-tool',
+];
 
 export const DEBOUNCE_INTERVAL_TIME = 500;
